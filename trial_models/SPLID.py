6fd87b41c37d9bb27c9e250926d005bb825d961a
OliverJMG <olivergnz1@gmail.com>
2024-01-16 16:03:51 +1300

    Tidyup

--- trial_models/SPLID.py
+++ trial_models/SPLID.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from torch.utils.data import Dataset
 
-
+# WIP pytorch class for SPLID dataset
 class SPLID(Dataset):
     def __init__(self, label_file, root_dir):
         self.node_labels = pd.read_csv(label_file)
